ce4fe19e694f8341a3e157e5f28af31ab7e07f37
George Cabal <george.cabal@nagarro.com>
2021-09-21 13:07:42 +0300

    Initial commit.

---
+++ quarkus-arango/src/test/java/org/nagarro/com/phonebook/NativeGreetingResourceIT.java
@@ -0,0 +1,9 @@
+package org.nagarro.com.phonebook;
+
+import io.quarkus.test.junit.NativeImageTest;
+
+@NativeImageTest
+public class NativeGreetingResourceIT extends GreetingResourceTest {
+
+    // Execute the same tests but in native mode.
+}
